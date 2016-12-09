ac4dd3dbb66cffbfde109ee49c8099f9d26a2881
David Flick <davemflick@yahoo.com>
2016-12-09 10:32:35 -0500

    Add files via upload

--- weather.js
+++ weather.js
@@ -3,6 +3,12 @@ $(document).ready(function() {
 	var country;
 	var $main = $('#main');
 
+  jQuery.ajaxPrefilter(function(options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+});
+
   $.ajax({
   	url: "http://ip-api.com/json",
   	dataType: 'json',
